6c0c89ef5a36065a6bc2c8216c79a157c694b47f
Konogan <konogan.bourhy@gmail.com>
2020-02-26 10:49:53 +0100

    conftest removal (for now)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,80 +1,80 @@
-"""conftest"""
-import random
+# """conftest"""
+# import random
 
-from genocrowd.app import create_app
+# from genocrowd.app import create_app
 
-import pytest
-
-
-@pytest.fixture
-def client():
-    """Summary
-
-    Returns
-    -------
-    TYPE
-        Description
-    """
-    client = Client()
-
-    yield client
+# import pytest
 
 
-class Client(object):
-    """Fixtrue class
+# @pytest.fixture
+# def client():
+#     """Summary
 
-    Attributes
-    ----------
-    app : TYPE
-        Description
-    client : TYPE
-        Description
-    config : TYPE
-        Description
-    ctx : TYPE
-        Description
-    db_path : TYPE
-        Description
-    """
+#     Returns
+#     -------
+#     TYPE
+#         Description
+#     """
+#     client = Client()
 
-    def __init__(self, config="config/genocrowd.test.ini"):
-        """Summary
+#     yield client
 
-        Parameters
-        ----------
-        config : str, optional
-            Description
-        """
-        # Config
-        self.config = config
-        self.db_path = "/tmp/database.db"
 
-        # create app
-        self.app = create_app(config=self.config)
-        # create_celery(self.app)
-        self.app.iniconfig.set('genocrowd', 'database_path', self.db_path)
+# class Client(object):
+#     """Fixtrue class
 
-        # context
-        self.ctx = self.app.app_context()
-        self.ctx.push()
+#     Attributes
+#     ----------
+#     app : TYPE
+#         Description
+#     client : TYPE
+#         Description
+#     config : TYPE
+#         Description
+#     ctx : TYPE
+#         Description
+#     db_path : TYPE
+#         Description
+#     """
 
-        # Client
-        self.client = self.app.test_client()
-        self.session = {}
+#     def __init__(self, config="config/genocrowd.test.ini"):
+#         """Summary
 
-    @staticmethod
-    def get_random_string(number):
-        """return a random string of n character
+#         Parameters
+#         ----------
+#         config : str, optional
+#             Description
+#         """
+#         # Config
+#         self.config = config
+#         self.db_path = "/tmp/database.db"
 
-        Parameters
-        ----------
-        number : int
-            number of character of the random string
+#         # create app
+#         self.app = create_app(config=self.config)
+#         # create_celery(self.app)
+#         self.app.iniconfig.set('genocrowd', 'database_path', self.db_path)
 
-        Returns
-        -------
-        str
-            a random string of n chars
-        """
-        alpabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-        return ''.join(random.choice(alpabet) for i in range(number))
+#         # context
+#         self.ctx = self.app.app_context()
+#         self.ctx.push()
+
+#         # Client
+#         self.client = self.app.test_client()
+#         self.session = {}
+
+#     @staticmethod
+#     def get_random_string(number):
+#         """return a random string of n character
+
+#         Parameters
+#         ----------
+#         number : int
+#             number of character of the random string
+
+#         Returns
+#         -------
+#         str
+#             a random string of n chars
+#         """
+#         alpabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+#         return ''.join(random.choice(alpabet) for i in range(number))
