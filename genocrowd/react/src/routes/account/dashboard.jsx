70e791c7729499783afaa04974ae06b9ed3f76c8
KonoganB <konogan.bourhy@gmail.com>
2020-06-17 14:58:07 +0200

    apollo version

--- genocrowd/react/src/routes/account/dashboard.jsx
+++ genocrowd/react/src/routes/account/dashboard.jsx
@@ -11,6 +11,8 @@ import {
   CardImg,
   CardHeader,
   Progress,
+  ListGroup,
+  ListGroupItem,
 } from "reactstrap";
 import PropTypes from "prop-types";
 import Identicon from "react-identicons";
@@ -34,59 +36,72 @@ export default class Dashboard extends Component {
       html = (
         <Container>
           <Row>
-            <Col>
-              <Card className="dashboard-cards">
-                <br></br>
-                <CardTitle tag="h3">{this.props.config.user.username}</CardTitle>
-                <CardSubtitle>GroupName</CardSubtitle>
-                <CardBody>
-                  
+            <Col xs="auto">
+              <Row>
+                <Card className="dashboard-cards">
                   <br></br>
-                  <Container></Container>
-                  <Progress value={2 * 5}></Progress>
-                  <br></br>
-                  <Identicon 
-                  size={100}
-                  string={this.props.config.user.username}
-
-                />
-                  
-                </CardBody>
-                
-              </Card>
-            </Col>
-            <Col>
+                  <CardTitle tag="h3">
+                    {this.props.config.user.username}
+                  </CardTitle>
+                  <CardSubtitle>GroupName</CardSubtitle>
+                  <CardBody>
+                    <br></br>
+                    <Container></Container>
+                    <Progress value={2 * 5}></Progress>
+                    <br></br>
+                    <Identicon
+                      size={100}
+                      string={this.props.config.user.username}
+                    />
+                  </CardBody>
+                </Card>
+              </Row>
               <Row>
-                <Card body outline className="dashboard-cards">
-                  <CardTitle>Project progress</CardTitle>
-                  <CardImg size="130%" src="../../../../static/logo/fauxcamembert.png"></CardImg>
-                  <hr></hr>
-                  <Button success>Get Started</Button>
+                <Card className="dashboard-cards">
+                  <ListGroup>
+                    <ListGroupItem>gene</ListGroupItem>
+                    <ListGroupItem>gene</ListGroupItem>
+                    <ListGroupItem>gene</ListGroupItem>
+                    <ListGroupItem>gene</ListGroupItem>
+                  </ListGroup>
                 </Card>
               </Row>
             </Col>
+            
             <Col>
-              <Row>
-                <Card className="dashboard-statcards">
-                <CardHeader>Annotated genes</CardHeader>
-                <CardBody>1000</CardBody>
-
-                </Card>
-              </Row>
-              <Row>
-                <Card className="dashboard-statcards">
-                <CardHeader>Number of annotators</CardHeader>
-                <CardBody>150</CardBody>
-
-                </Card>
-              </Row>
-              <Row>
-                <Card className="dashboard-statcards">
-                <CardHeader>Number of groups</CardHeader>
-                <CardBody>15</CardBody>
-
-                </Card>
-              </Row>
+              <Col>
+                <Row>
+                  <Card body outline className="dashboard-cards">
+                    <CardTitle>Project progress</CardTitle>
+                    <CardImg
+                      size="130%"
+                      src="../../../../static/logo/fauxcamembert.png"
+                    ></CardImg>
+                    <hr></hr>
+                    <Button success>Get Started</Button>
+                  </Card>
+                </Row>
+              </Col>
+              <Col>
+                <Row>
+                  <Card className="dashboard-statcards">
+                    <CardHeader>Annotated genes</CardHeader>
+                    <CardBody>1000</CardBody>
+                  </Card>
+                </Row>
+                <Row>
+                  <Card className="dashboard-statcards">
+                    <CardHeader>Number of annotators</CardHeader>
+                    <CardBody>150</CardBody>
+                  </Card>
+                </Row>
+                <Row>
+                  <Card className="dashboard-statcards">
+                    <CardHeader>Number of groups</CardHeader>
+                    <CardBody>15</CardBody>
+                  </Card>
+                </Row>
+              </Col>
             </Col>
           </Row>
         </Container>
