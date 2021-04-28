e36dabc2ab36f3a7c3eab9d2fc61e37e7dbeab0e
CoralieFouque <coralie.fouque@sfr.fr>
2021-04-28 08:33:47 +0000

    add test

--- tests/test_api_data.py
+++ tests/test_api_data.py
@@ -2,14 +2,34 @@ from . import GenocrowdTestCase
 
 
 class TestApiData(GenocrowdTestCase):
+    """Test AskOmics API /api/data/<something>"""
 
-    def test_update_groupsAmount_ok(self, client):
+    def test_update_groups_amount_ok(self, client):
         client.create_two_users()
         client.log_user("jdoe")
         ok_data = {
             "newNumber": 2
         }
+        """Test update number of groups"""
         response = client.client.post('api/data/setgroupsamount', json=ok_data)
         assert response.status_code == 200
         assert response.json["error"] is False
         assert response.json["groupsAmount"]["updatedExisting"] is True
+
+        """Test creation group"""
+        response2 = client.client.get('/api/admin/getgroups')
+        assert len(response2.json["groups"]) == ok_data["newNumber"]
+
+    def test_group_assignement(self, client):
+        client.create_two_users()
+        client.log_user("jdoe")
+        ok_data = {
+            "groupsAmount": 2
+        }
+
+        response = client.client.post('/api/admin/setgroup', json=ok_data)
+        response2 = client.client.get('/api/admin/getusers')
+        response3 = client.client.get('/api/admin/getgroups')
+        assert response.status_code == 200 and response2.status_code == 200 and response3.status_code == 200
+        assert response.json["error"] is False and response2.json["error"] is False and response3.json["error"] is False
+        assert response2.json["users"][1]["_id"] == response3.json["groups"][0]["student"][0]["_id"]
