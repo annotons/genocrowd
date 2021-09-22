34b0f1122458881b6fd625b541ded68c996b087d
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2021-09-22 17:43:46 +0200

    todo

--- genocrowd/api/auth/login.py
+++ genocrowd/api/auth/login.py
@@ -157,12 +157,13 @@ def update_password():
     return result
 
 
+# TODO check if this should be restricted to admin
 @auth_bp.route('/api/auth/levelup', methods=['POST'])
 @login_required
 def user_level_up():
     data = request.get_json()
     local_auth = LocalAuth(ca, session)
-    result = local_auth.user_level_up(data['annotator'])
+    result = local_auth.user_level_up(data['annotator'])  # TODO not implemented (lost?)
 
     return result
 
